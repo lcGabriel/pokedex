de65ee80cd205a847479a29c95adb53840af78fa
lcGabriel <reidney.lg@gmail.com>
2022-02-08 02:10:21 -0300

    inserting search pokemon on app

--- app/src/main/java/br/com/lj/pokedex_android/PokemonListActivity.kt
+++ app/src/main/java/br/com/lj/pokedex_android/PokemonListActivity.kt
@@ -1,23 +1,25 @@
 package br.com.lj.pokedex_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
+import android.widget.ArrayAdapter
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import br.com.lj.pokedex_android.api.PokemonRepository
+import br.com.lj.pokedex_android.databinding.ActivityMainBinding
 import br.com.lj.pokedex_android.domain.Pokemon
-import br.com.lj.pokedex_android.domain.PokemonType
 import br.com.lj.pokedex_android.view.PokemonAdapter
 import br.com.lj.pokedex_android.viewModel.PokemonViewModel
 import br.com.lj.pokedex_android.viewModel.PokemonViewModelFactory
+import java.util.*
 
 class PokemonListActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var recyclerView: RecyclerView
+    private  var listPokemon: List<Pokemon>? = null
 
     private val viewModel by lazy {
         ViewModelProvider(this, PokemonViewModelFactory())
@@ -26,12 +28,32 @@ class PokemonListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        recyclerView = findViewById(R.id.rv_pokemons)
+        recyclerView = binding.rvPokemons
 
-        viewModel.pokemons.observe(this, Observer {
+        viewModel.pokemons.observe(this) {
             loadRecyclerView(it)
+            searchPokemons(it)
+        }
+    }
+
+    private fun searchPokemons(pokemons: List<Pokemon?>) {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean = false
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val filteredContacts =
+                    pokemons.filter { newText!!.uppercase(Locale.getDefault()) in it!!.name.uppercase(
+                        Locale.getDefault()
+                    ) }
+
+                recyclerView.adapter = PokemonAdapter(filteredContacts)
+
+                return false
+            }
         })
     }
 
